65b3144b30d36ce0eb80aceff1f4b6c1cc065b1e
Mohanraj M <max.neo.dev@gmail.com>
2024-04-25 10:12:42 +0530

    Dev 19 : Signup Integration

--- max-quiz-react/src/pages/Auth/Register.jsx
+++ max-quiz-react/src/pages/Auth/Register.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { SignUp } from '../../services/api'
 
 const Register = () => {
+
     const navigate = useNavigate()
+    const [registerdata, setRegisterdata] = useState({
+        "name": '',
+        "phone": '',
+        "address": '',
+        "email": '',
+        "password": ''
+    })
+    const handleChange = (e) => {
+        setRegisterdata({ ...registerdata, [e.target.id]: e.target.value })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(registerdata);
+        const res = await SignUp(registerdata);
+        // console.log(res.status);
+
+    }
     return (
         <>
             <div className='p-0 m-0 h-[90vh] w-screen flex justify-center items-center flex-col'>
-                <form className='flex flex-col gap-5 bg-slate-50/80 h-3/6 w-[30%] items-center justify-center rounded-md shadow-md shadow-orange-100'>
-                    <input type="text" name="" id="" placeholder='Name' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' />
-                    <input type="number" name="" id="" placeholder='Phone' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' />
-                    <input type="email" name="" id="" placeholder='Email' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' />
-                    <input type="password" name="" id="" placeholder='Password' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' />
+                <form className='flex flex-col gap-5 bg-slate-50/80 h-4/6 w-[30%] items-center justify-center rounded-md shadow-md shadow-orange-100' onSubmit={handleSubmit}>
+                    <input type="text" name="" id="name" placeholder='Name' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' onChange={handleChange} required />
+                    <input type="text" name="" id="phone" placeholder='Phone' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' onChange={handleChange} required />
+                    <input type="text" name="" id="address" placeholder='Address' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' onChange={handleChange} required />
+                    <input type="email" name="" id="email" placeholder='Email' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' onChange={handleChange} required />
+                    <input type="password" name="" id="password" placeholder='Password' className='bg-orange-100/50 outline-none border-2 border-transparent focus:border-b-2 focus:border-b-orange-300 rounded-sm w-[80%] text-black placeholder:text-black p-2 shadow-sm' onChange={handleChange} required />
                     <button type="submit" className='w-[80%] bg-gradient-to-tr from-orange-600 to-orange-300 text-white p-2 rounded-sm font-bold mt-4 shadow-md shadow-orange-500/40'>Register</button>
                     <p>Already have an account ? <span className='text-orange-500 cursor-pointer font-bold' onClick={() => navigate('/login')}> Login ! </span></p>
                 </form>
--- max-quiz-react/src/services/api.jsx
+++ max-quiz-react/src/services/api.jsx
@@ -1,1 +1,23 @@
 // test
+
+
+import axios from 'axios'
+
+
+const URI = 'http://localhost:7777/api'
+
+const SignIn = (logindata) => axios.post(URI + '/auth/login', { logindata })
+const SignUp = (registerdata) => axios.post(URI + '/auth/register', { registerdata })
+
+// const logindata ={
+//     email:'',
+//     password:''
+// }
+// const registerdata ={
+//     name:'',
+//     phone:"0",
+//     address:'',
+//     email:'',
+//     password:''
+// }
+export { SignIn, SignUp }
